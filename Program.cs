589d9e9fe5f06a9dd79d314c91e69744f8b03ca8
sefikanurtopuz <sefikanurtopuz@gmail.com>
2023-01-26 14:35:37 +0300

    hataların düzeltilmesi gerekiyor

--- Program.cs
+++ Program.cs
@@ -19,36 +19,38 @@ namespace seçilen_işlemi_yaptırma
             int islem = Int32.Parse(Console.ReadLine());
             if (islem == 1)
             {
-                Console.WriteLine(sayi1 + sayi2);
+                Console.WriteLine(sayi1 + sayi2); // Console.WriteLine("Toplama işlemi sonucu: " + (sayi1 + sayi2).ToString()) şeklinde olabilirdi
                 Console.ReadLine();
             }
             else if (islem == 2)
             {
-                Console.WriteLine(sayi1 - sayi2);
+                Console.WriteLine(sayi1 - sayi2); //Console.WriteLine("Çıkarma işlemi sonucu:" + (sayi1 - sayi2).ToString()) şeklinde olabilirdi
                 Console.ReadLine();
             }
             else if (islem == 3)
             {
-                Console.WriteLine(sayi1 * sayi2);
+                Console.WriteLine(sayi1 * sayi2); // Console.WriteLine("Çarpma işlemi sonucu: " + (sayi1 * sayi2).ToString()) şeklinde olabilirdi
                 Console.ReadLine();
             }
             else if (islem == 4)
             {
-                Console.WriteLine(sayi1 / sayi2);
+                Console.WriteLine(sayi1 / sayi2); // Console.WriteLine("Bölme işleminin sonucu:" + (sayi1 / sayi2).ToString()) şeklinde olabilirdi //bölme işlemindeki hatalar burdada geçerli
                 Console.ReadLine();
             }
             else if (islem == 5)
             {
-                Console.WriteLine(sayi1 % sayi2);
+                Console.WriteLine(sayi1 % sayi2); //Console.WriteLine("Mod bulma işleminin sonucu:" + (sayi1 % sayi2).ToString()) şeklinde olabilirdi
                 Console.ReadLine();
             }
             else if (islem == 6)
-            { Console.WriteLine(sayi1 * sayi1);
-                Console.WriteLine(sayi2 * sayi2);
+            { 
+                Console.WriteLine(sayi1 * sayi1); //Console.WriteLine("Birinci sayının karesi: " + (sayi1 * sayi1).ToString()) şeklinde olabilirdi
+                Console.WriteLine(sayi2 * sayi2); //Console.WriteLine("İkinci sayının karesi: " + (sayi2 * sayi2).ToString()) şeklinde olabilirdi
                 Console.ReadLine();
             }
             else if (islem == 7)
-            { Console.WriteLine((sayi1 + sayi2) / 2);
+            { 
+                Console.WriteLine((sayi1 + sayi2) / 2); //Console.WriteLine("Sayıların ortalaması: " + ((sayi1 + sayi2) / 2).ToString()) şeklinde olabilirdi // 5 ile 8 in ortalaması 6.5 ama sonuç 6 çıkıyor
                 Console.ReadLine();
             }
         } 
